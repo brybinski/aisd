02174ba9e926839cb0a73339a63276690b9c3f01
Bartosz Rybiński <bartoszrybinski@outlook.com>
2021-10-15 01:54:32 +0200

    Good morning to future myself!

--- Lab02.py
+++ Lab02.py
@@ -17,6 +17,8 @@ class LinkedList:
         newNode: Node = Node(value)
         newNode.next = self.head
         self.head = newNode
+        if self.tail is None:
+            self.tail = newNode
 
     def append(self, value) -> None:
         newNode: Node = Node(value)
@@ -24,33 +26,68 @@ class LinkedList:
         self.tail = newNode
 
     def print(self) -> None:
-        tmp: 'Node' = self.head
-        while True:
-            print(tmp.value)
-            if tmp == self.tail:
-                break
-            tmp = tmp.next
+        if self.head is not None:
+            result: str = str(self.head.value)
+            if self.head.next is None:
+                print(result)
+                return
+            else:
+                tmp: 'Node' = self.head
+                while tmp.next is not self.tail:
+                    tmp = tmp.next
+                    result += "->" + str(tmp.value)
+            print(result)
+        else:
+            print("None")
 
     def len(self) -> int:
         i: int = 1
+        if self.head is None:
+            return 0
+
         tmp: Node = self.head
-        while tmp != self.tail
+        while tmp != self.tail:
             i += 1
             tmp = tmp.next
         return i
 
+    def insert(self, value: Any, after: Node) -> None:
+        tmp = after.next
+        after.next = Node(value)
+        after.next.next = tmp
 
     def node(self, at: int) -> Node:
         result: Node = self.head
         i: int = 1
         if at <= self.len():
             while i != at:
-                i +=1
+                result = result.next
+                i += 1
             return result
         else:
-            print("Index out of bounds, LinkedList.len() is " self.len()+ "")
+            print(f"Index out of bounds, LinkedList.len() is {self.len()} but {at} was given")
+
+    def pop(self) -> Any:
+        result: int = self.head
+        if self.head != self.tail:
+            self.head = self.head.next
+        else:
+            self.head = None
+            self.tail = None
+        return result
+
 
 ll: LinkedList = LinkedList(2137)
 ll.push(420)
 ll.append(69)
+ll.insert("xD", ll.node(2))
+ll.print()
+print(ll.node(2).value)
+ll.pop()
+ll.pop()
+ll.pop()
+ll.pop()
+print(ll.len())
+ll.print()
+ll.push(911)
 ll.print()
